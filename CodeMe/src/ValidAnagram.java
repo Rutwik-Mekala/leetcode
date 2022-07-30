281e5d0210f0154284a6ca432ae83f0a8655ee4b
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 20:42:43 +0530

    solved answer1

--- CodeMe/src/ValidAnagram.java
+++ CodeMe/src/ValidAnagram.java
@@ -1,3 +1,20 @@
-public class ValidAnagram {
-
+import java.util.*;
+class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(answer1(s,t));
+    }
+    public static boolean answer1(String s,String t){
+        if(s.length()!=t.length()) return false;
+        int[] count = new int[26];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i]!=0) return false;
+        }
+        return true;
+    }
 }
