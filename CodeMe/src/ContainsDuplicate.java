afea3e6f751b88b2ae0e1b90c626b56f427c7583
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 20:32:05 +0530

    uploading solution

---
+++ CodeMe/src/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(answer2(nums));
+    }
+    public static boolean answer1(int[] nums){
+        Arrays.sort(nums);
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
+    public static boolean answer2(int[] nums){
+        HashSet<Integer> s = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(s.contains(nums[i])) return true;
+            s.add(nums[i]);
+        }
+        return false;
+    }
+}
