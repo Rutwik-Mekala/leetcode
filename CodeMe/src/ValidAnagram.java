33783d54473510ab592a24ae81480756e84ab458
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 20:46:00 +0530

    solved with alternate approach. problem solved

--- CodeMe/src/ValidAnagram.java
+++ CodeMe/src/ValidAnagram.java
@@ -3,7 +3,7 @@ class ValidAnagram {
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
-        System.out.println(answer1(s,t));
+        System.out.println(answer2(s,t));
     }
     public static boolean answer1(String s,String t){
         if(s.length()!=t.length()) return false;
@@ -14,6 +14,19 @@ class ValidAnagram {
         }
         for(int i=0; i<26; i++){
             if(count[i]!=0) return false;
+        }
+        return true;
+    }
+    public static boolean answer2(String s,String t){
+        if(s.length()!=t.length()) return false;
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        for(int i=0; i<s.length(); i++){
+            count1[s.charAt(i)-'a']++;
+            count2[t.charAt(i)-'a']++;
+        }
+        for(int i=0; i<26; i++){
+            if(count1[i]!=count2[i]) return false;
         }
         return true;
     }
