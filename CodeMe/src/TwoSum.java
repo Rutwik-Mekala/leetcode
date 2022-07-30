d551f4109bcb91a0b8ef2d43fbde879d40678ea2
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 21:26:01 +0530

    updated with slightly different solution on neetcode

--- CodeMe/src/TwoSum.java
+++ CodeMe/src/TwoSum.java
@@ -1,19 +1,33 @@
 import java.util.*;
+
 class TwoSum {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
-        for(int i: answer(nums,target)){
-            System.out.print(i+" ");
+        for (int i : answer(nums, target)) {
+            System.out.print(i + " ");
         }
     }
-    public static int[] answer(int[] nums, int target){
-        HashMap<Integer,Integer> m = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(m.containsKey(target-nums[i])){
-                return new int[]{m.get(target-nums[i]),i};
+    public static int[] answer(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (m.containsKey(target - nums[i])) {
+                return new int[]{m.get(target - nums[i]), i};
             }
-            m.put(nums[i],i);
+            m.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static int[] neetcodeSolution(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+            if (prevMap.containsKey(nums[i])) {
+                return new int[]{prevMap.get(num), i};
+            }
+            prevMap.put(target - num, i);
         }
         return new int[]{};
     }
