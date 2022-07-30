31793652c2479a7c0ba6729413c6bd3a5c8c3b33
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 21:22:21 +0530

    got 3ms final solution

--- CodeMe/src/TwoSum.java
+++ CodeMe/src/TwoSum.java
@@ -3,9 +3,18 @@ class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
-        System.out.println(Arrays.asList(answer(nums,target)));
+        for(int i: answer(nums,target)){
+            System.out.print(i+" ");
+        }
     }
     public static int[] answer(int[] nums, int target){
-
+        HashMap<Integer,Integer> m = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(m.containsKey(target-nums[i])){
+                return new int[]{m.get(target-nums[i]),i};
+            }
+            m.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
