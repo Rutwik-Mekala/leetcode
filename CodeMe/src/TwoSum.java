bc3b40d8acdb5d29d83793846a1d7920234b600d
RutwikMekala <imrutwik99@gmail.com>
2022-07-30 21:11:39 +0530

    configured program to leetcode style

---
+++ CodeMe/src/TwoSum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.asList(answer(nums,target)));
+    }
+    public static int[] answer(int[] nums, int target){
+
+    }
+}
